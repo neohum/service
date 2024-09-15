65ed7d7452d5793764bb371a99e1c6a98a573b23
neohum <neohum77@gmail.com>
2024-09-15 19:15:46 +0900

    main page001 (#1)

---
+++ resources/views/rents/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('content')
+<div class="row h-100 align-items-center justify-content-center">
+    <div class="col-lg-8">
+        <div class="hero-text-content text-center">
+            <h6> 웹에서 기기를 신청하고 사용 후 반납 기록을 해보세요</h6>
+            <h3>
+                - IT 기기 대여 서비스 -
+            </h3>
+            <a class="btn hero-btn mt-50" href="#">서비스 신청하기</a>
+        </div>
+    </div>
+</div>
+@endsection
